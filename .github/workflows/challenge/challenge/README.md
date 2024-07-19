cc6e826a0c47a20c647d6665fa5309fbc14b2039
Danika Pearson <89046934+karmadog72@users.noreply.github.com>
2024-07-19 14:22:12 -0600

    Add files via upload

---
+++ .github/workflows/challenge/challenge/README.md
@@ -0,0 +1,5 @@
+# Instructions
+
+- Clone this repo and run the react app (git clone, npm update, npm start)
+- Complete cards in Trello
+- Publish to a new branch in your own github. 
---
+++ .github/workflows/challenge/challenge/src/index.js
@@ -0,0 +1,8 @@
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import api from './api.js';
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
